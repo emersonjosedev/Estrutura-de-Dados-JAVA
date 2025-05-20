ea355d8b70c46e52b5b0b0ac05748a2596e371b1
brucarv <brucarv.nah@gmail.com>
2025-05-20 12:50:50 -0300

    0.01

---
+++ src/Calculo.java
@@ -0,0 +1,29 @@
+public class Calculo {
+    //Variáveis e tipos de Dados – em Java
+    int idade = 10;
+    char sexo = 'F';
+    String nome = "Emerson";
+
+  //função
+
+    public static int somar(int a, int b){
+        return a+ b;
+    }
+    class Pessoa{
+        String nome;
+        int idade;
+
+    }
+    public static void chovendo(boolean chuva){
+
+        //Estrutura de Controle
+        if(chuva){
+            System.out.println("Esta chovendo");
+        }
+        else {
+
+            System.out.println("Não está chovendo");
+
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculo c = new Calculo();
+         Calculo c2 = new Calculo();
+        System.out.println(c.idade);
+        c.chovendo(true);
+        c2.chovendo(false);
+
+    }
+}
